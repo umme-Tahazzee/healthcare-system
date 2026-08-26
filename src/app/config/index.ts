de05d358ba43f626b6bd802ebb2dbe536138d147
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-08-26 12:46:43 +0600

    feat: inital setup for redis

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -25,7 +25,12 @@ export default {
 
 	tester_doctor_name: process.env.TESTER_DOCTOR_NAME!,
 	tester_doctor_email:process.env.TESTER_DOCTOR_EMAIL!,
-	tester_doctor_password:process.env.tester_doctor_password!
+	tester_doctor_password:process.env.tester_doctor_password!,
+
+	redis_user:process.env.REDIS_USER!,
+	redis_password:process.env.REDIS_PASSWORD!,
+	redis_host:process.env.REDIS_HOST!,
+	redis_port:process.env.REDIS_PORT!,
 
 
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import config from "./app/config";
 import { prisma } from "./app/lib/prisma";
+import { redisClient } from "./app/lib/redis";
 import { seedSuperAdmin, seedTeasterAdmin, seedTeasterDoctor } from "./app/utils/seed";
 
 const PORT = config.port;
@@ -8,10 +9,13 @@ const PORT = config.port;
 const main = async () => {
 	try {
 		await prisma.$connect();
+		console.log("Connected to the database successfully.");
+		await redisClient.connect()
+		console.log("Redis connected successfully");
+		
 		await seedSuperAdmin()
 		await  seedTeasterAdmin()
 		await seedTeasterDoctor()
-		console.log("Connected to the database successfully.");
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
